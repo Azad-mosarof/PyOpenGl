5f0fed518c0aafce0872127ab53a7b600f0f06ab
Azad-mosarof <misarofazad@gmail.com>
2023-03-07 18:06:47 +0300

    update

--- SpinningCube/cube.py
+++ SpinningCube/cube.py
@@ -2,29 +2,59 @@ import pygame as pg
 import numpy as np
 from OpenGL.GL import *
 import ctypes
+import pyrr
 from OpenGL.GL.shaders import compileProgram, compileShader
 
-vertext_file_path = "/home/azadm/Desktop/openGl/PyOpenGl/Hands_On_OpenGL/shaders/vertext.txt"
-fragment_file_path = "/home/azadm/Desktop/openGl/PyOpenGl/Hands_On_OpenGL/shaders/fragment.txt"
-image_path_1 = "/home/azadm/Desktop/openGl/PyOpenGl/Hands_On_OpenGL/assests/cat.png"
-image_path_2 = "/home/azadm/Desktop/openGl/PyOpenGl/Hands_On_OpenGL/assests/wood.jpeg"
-obj_path = "/home/azadm/Desktop/openGl/PyOpenGl/Hands_On_OpenGL/assests/cube.obj"
+vertext_file_path = "/home/azadm/Desktop/openGl/PyOpenGl/SpinningCube/vertex.txt"
+fragment_file_path = "/home/azadm/Desktop/openGl/PyOpenGl/SpinningCube/fragment.txt"
+image_path_1 = "/home/azadm/Desktop/openGl/PyOpenGl/assets/images/cat.png"
+image_path_2 = "/home/azadm/Desktop/openGl/PyOpenGl/assets/images/wood.jpeg"
+obj_path = "/home/azadm/Desktop/openGl/PyOpenGl/assets/models/cube.obj"
+
+display_width = 640
+display_height = 480
+
+class Cube:
+
+    def __init__(self, position, eulers):
+        self.position = np.array(position, dtype=np.float32)
+        self.eulers = np.array(eulers, dtype=np.float32)
 
 class App:
 
     def __init__(self):
         pg.init()
-        pg.display.set_mode((600, 400), pg.OPENGL | pg.DOUBLEBUF)
+        pg.display.gl_set_attribute(pg.GL_CONTEXT_MAJOR_VERSION, 3)
+        pg.display.gl_set_attribute(pg.GL_CONTEXT_MINOR_VERSION, 3)
+        pg.display.gl_set_attribute(pg.GL_CONTEXT_PROFILE_MASK,
+                                        pg.GL_CONTEXT_PROFILE_CORE)
+        pg.display.set_mode((display_width, display_height), pg.OPENGL | pg.DOUBLEBUF)
         self.clock = pg.time.Clock()
-        glClearColor(0.1, 0.2, 0.2, 3)
-        glEnable(GL_BLEND)
-        glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA)
+        glClearColor(0.1, 0.2, 0.2, 1)
+        self.cube_mesh = Mesh(obj_path)
         self.shader = self.createShader(vertext_file_path, fragment_file_path)
         glUseProgram(self.shader)
         glUniform1i(glGetUniformLocation(self.shader, "imageTexture"), 0)
-        self.triangle = Triangle()
-        # self.triangle = Mesh(obj_path)
-        self.wood_texture = Material(image_path_1)
+        glEnable(GL_DEPTH_TEST)
+
+        
+        self.wood_texture = Material(image_path_2)
+
+        self.cube = Cube(
+            position=[0,0,-3],
+            eulers=[0,0,0]
+        )
+
+        projection_transform = pyrr.matrix44.create_perspective_projection(
+            fovy=75, aspect=display_width/display_height,
+            near=0.1, far=10, dtype=np.float32
+        )
+
+        glUniformMatrix4fv(
+            glGetUniformLocation(self.shader, "projection"),
+            1, GL_FALSE, projection_transform
+        )
+        self.modelMatrixLocation = glGetUniformLocation(self.shader, "model")
         self.mainLoop()
 
     def createShader(self, vertexFilePath, fragmentFilePath):
@@ -48,16 +78,42 @@ class App:
             for event in pg.event.get():
                 if(event.type == pg.QUIT):
                     running = False
+
+            #update the cube
+            self.cube.eulers[2] += 0.25
+            if self.cube.eulers[2] > 360:
+                self.cube.eulers[2] -= 360
+
             #refresh screen
-            glClear(GL_COLOR_BUFFER_BIT)  
-
+            glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)  
             glUseProgram(self.shader)
-            self.wood_texture.use()
-            glBindVertexArray(self.triangle.vao)
-            glDrawArrays(GL_TRIANGLES, 0, self.triangle.vertex_count)
 
-            """glDrawArrays functions take the argument draw mode, starting position
-            and number of points"""
+            model_transform = pyrr.matrix44.create_identity(dtype=np.float32)
+
+            """
+                pitch: rotation around x axis
+                roll: rotation around z axis
+                yaw: roatation around y axis
+            """
+
+            model_transform = pyrr.matrix44.multiply(
+                m1=model_transform,
+                m2=pyrr.matrix44.create_from_eulers(
+                    eulers=np.radians(self.cube.eulers), dtype=np.float32
+                )
+            )
+
+            model_transform = pyrr.matrix44.multiply(
+                m1=model_transform, 
+                m2=pyrr.matrix44.create_from_translation(
+                    vec=np.array(self.cube.position),dtype=np.float32
+                )
+            )
+
+            glUniformMatrix4fv(self.modelMatrixLocation,1,GL_FALSE,model_transform)
+            self.wood_texture.use()
+            glBindVertexArray(self.cube_mesh.vao)
+            glDrawArrays(GL_TRIANGLES, 0, self.cube_mesh.vertex_count)
 
             pg.display.flip()
 
@@ -66,106 +122,10 @@ class App:
         self.quit()
 
     def quit(self):
-        self.triangle.destroy()
+        self.cube_mesh.destroy()
         self.wood_texture.destroy()
         glDeleteProgram(self.shader)
         pg.quit()
-
-class Triangle:
-
-    def __init__(self):
-        #x, y, z, r, g, b, s, t
-        self.vertices = (
-            -0.5, -0.5, 0.0, 1.0, 0.0, 0.0, 0.0, 1.0,
-            0.5, -0.5, 0.0, 0.0, 1.0, 0.0, 1.0, 1.0,
-            0.0, 0.5, 0.0, 0.0, 0.0, 1.0, 0.5, 0.0
-        )
-
-        self.vertices = np.array(self.vertices, dtype=np.float32)
-        self.vertex_count = 3
-
-        self.vao = glGenVertexArrays(1)
-        glBindVertexArray(self.vao)
-
-        """
-        vertex array object is the best way to represent the
-        vertex data.
-        """
-
-        self.vbo = glGenBuffers(1)  #it will create one open opengl buffer object. buffer is the basic storage container
-        glBindBuffer(GL_ARRAY_BUFFER, self.vbo) #bind that buffer 
-        glBufferData(GL_ARRAY_BUFFER, self.vertices.nbytes, self.vertices, GL_STATIC_DRAW)
-
-        """
-        glBufferData used to shift the buffer data to
-        the graphics card. We tell openGl where we want to store 
-        the data, how many bytes we are sending through, then the data
-        and then we are telling the opengl how to use the data
-        """
-
-        glEnableVertexAttribArray(0)
-        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 32, ctypes.c_void_p(0))
-        glEnableVertexAttribArray(1)
-        glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 32, ctypes.c_void_p(12))
-        glEnableVertexAttribArray(2)
-        glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 32, ctypes.c_void_p(24))
-
-        """
-        Here we first enabled the attributes. Attributes 0 is for the 
-        position x, y, z and the attribute 1 is for the rgb color channels.
-        each attribute have total 3 points and data types is float. GL_FALSE
-        means it openGL does not need any extra work to normalize the data.
-        next argument is the stride that how many steps it need to jump to go 
-        to the next vertices. each verices have 6 number, so 6*4 = 24. Next
-        argument is the offset that where the is the starting point of the data.
-        the starting position of for the first vertex is 0 and the starting
-        position for the first color point is 3*4 = 12(after the 3 vertex).
-
-        Here we actually allocate some memory in the graphics card.So after
-        compleating the task we should clear the memory.
-        """
-
-    def destroy(self):
-        glDeleteVertexArrays(1, (self.vao,))
-        glDeleteBuffers(1, (self.vbo,))
-
-    """In order to draw anything first we need to make shaders.We need
-    Two shaders, one is vertex shader and another one is fragment shader.
-    The vertex shader runs for per vertex, responsible to set the position
-    and make transformation.Then the shape is assembled and break down
-    into fragments or pixels.The fragment shader run ones per pixels
-    and it is responsible for calculating the color that pixel should
-    take."""
-
-class CubeMesh:
-
-    def __init__(self):
-        #x, y, z, r, g, b, s, t
-        self.vertices = (
-            -0.5, -0.5, 0.0, 0.0, 1.0,
-            0.5, -0.5, 0.0, 1.0, 1.0,
-            0.0, 0.5, 0.0, 0.5, 0.0
-        )
-
-        self.vertices = np.array(self.vertices, dtype=np.float32)
-        self.vertex_count = 3
-
-        self.vao = glGenVertexArrays(1)
-        glBindVertexArray(self.vao)
-
-        self.vbo = glGenBuffers(1)  #it will create one open opengl buffer object. buffer is the basic storage container
-        glBindBuffer(GL_ARRAY_BUFFER, self.vbo) #bind that buffer 
-        glBufferData(GL_ARRAY_BUFFER, self.vertices.nbytes, self.vertices, GL_STATIC_DRAW)
-
-        glEnableVertexAttribArray(0)
-        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 20, ctypes.c_void_p(0))
-        glEnableVertexAttribArray(1)
-        glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 20, ctypes.c_void_p(12))
-
-
-    def destroy(self):
-        glDeleteVertexArrays(1, (self.vao,))
-        glDeleteBuffers(1, (self.vbo,))
 
 
 class Mesh:
@@ -245,7 +205,7 @@ class Mesh:
 
         for i in range(triangleCount):
             self.make_corner(words[1], v, vt, vn, vertices)
-            self.make_corner(words[2+1], v, vt, vn, vertices)
+            self.make_corner(words[2+i], v, vt, vn, vertices)
             self.make_corner(words[3+i], v, vt, vn, vertices)
     
     def make_corner(
@@ -294,7 +254,7 @@ class Material:
         specifies the filtering mode, which in this case is linear filtering. Linear filtering takes the average of surrounding 
         texels to produce a smooth appearance, which is better suited for larger texture scales."""
 
-        image = pg.image.load(filePath).convert_alpha()
+        image = pg.image.load(filePath).convert()
         image_width, image_height = image.get_rect().size
         image_data = pg.image.tostring(image, "RGBA") #opengl does not understand the pygame image data.that's we convert the image data into string
         glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, image_width, image_height, 0, GL_RGBA, GL_UNSIGNED_BYTE, image_data)
@@ -306,7 +266,6 @@ class Material:
 
     def destroy(self):
         glDeleteTextures(1, (self.texture,))
-
 
 
 if __name__ == "__main__":
